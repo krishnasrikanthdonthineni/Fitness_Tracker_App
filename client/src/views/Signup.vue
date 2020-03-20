cd05018ae2e67aa9dbebc00d7b2f5a5c3645b66a
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2020-03-20 16:33:01 -0400

    update signUP

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,10 +1,108 @@
 <template>
-  <section class="container">
-    <div class="hero">
-        <h1 class="title">
-          Sign Up
-        </h1>
+  <div class="section">
+    <div class="container sign-up-container">
+      <div class="panel">
+        <p class="panel-heading">Sign up</p>
+        <div class="panel-block">
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control has-icons-left">
+              <input type="text" class="input" placeholder="Your username" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="panel-block">
+          <div class="field">
+            <label class="label">First name</label>
+            <div class="control">
+              <div class="control has-icons-left">
+                <input type="text" class="input" placeholder="Your last name" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+              </div>
+            </div>
+            <label class="label">Last name</label>
+            <div class="control">
+              <div class="control has-icons-left">
+                <input type="text" class="input" placeholder="Your first name" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-block">
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control has-icons-left">
+              <input type="email" class="input" placeholder="Your email" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="panel-block">
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control has-icons-left">
+              <input type="password" class="input" placeholder="Your password" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+              </span>
+            </div>
+            <label class="label">Repeat password</label>
+            <div class="control has-icons-left">
+              <input type="password" class="input" placeholder="Confirm your passowrd" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="panel-block">
+          <div class="file has-name is-fullwidth w-100">
+            <label class="file-label">
+              <input class="file-input" type="file" name="resume" />
+              <span class="file-cta">
+                <span class="file-icon">
+                  <i class="fas fa-upload"></i>
+                </span>
+                <span class="file-label">Choose a profile picture...</span>
+              </span>
+              <span class="file-name">Screen Shot 2017-07-29 at 15.54.25.png</span>
+            </label>
+          </div>
+        </div>
+        <div class="panel-block">
+          <div class="field">
+            <div class="control">
+              <a class="button is-primary w-100">Register</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-  </section>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "SignUp"
+};
+</script>
+
+<style scoped>
+.sign-up-container {
+  max-width: 40vw;
+}
+.w-100,
+.field {
+  width: 100%;
+}
+</style>
